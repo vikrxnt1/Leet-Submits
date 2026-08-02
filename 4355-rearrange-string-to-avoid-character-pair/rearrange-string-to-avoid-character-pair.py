44de6b878ea46d90ba643a0f97160ece4c1a4206
Vikrant Singh <113819133+vikrxnt1@users.noreply.github.com>
2026-08-03 00:12:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (34.84%) - LeetSync

--- 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.py
+++ 4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.py
@@ -1,16 +1,28 @@
 class Solution:
     def rearrangeString(self, s: str, x: str, y: str) -> str:
-        d = {}
+        # Dictionary Solution Time -> O(n) Space -> O(n)
+        
+        # d = {}
+        # for i in s:
+        #     if i in d:
+        #         d[i] += 1
+        #     else:
+        #         d[i] = 1
+        # if x not in d or y not in d:
+        #     return s
+        # ans = y*d[y] + x*d[x]
+        # del d[x]
+        # del d[y]
+        # for a,b in d.items():
+        #     ans += a*b
+        # return ans
+
+        # Stack Solution Time -> O(n) Space -> O(1)
+        ans = ""
+        st = ""
         for i in s:
-            if i in d:
-                d[i] += 1
+            if i == x:
+                st += x
             else:
-                d[i] = 1
-        if x not in d or y not in d:
-            return s
-        ans = y*d[y] + x*d[x]
-        del d[x]
-        del d[y]
-        for a,b in d.items():
-            ans += a*b
-        return ans
+                ans += i
+        return ans + st
