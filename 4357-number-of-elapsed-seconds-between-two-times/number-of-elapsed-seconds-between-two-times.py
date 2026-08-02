c7dce1158799c24d86b00c418ebdc9ce8140ca6f
Vikrant Singh <113819133+vikrxnt1@users.noreply.github.com>
2026-08-03 00:52:57 +0530

    Time: 1 ms (33.11%) | Memory: 19.2 MB (90.14%) - LeetSync

--- 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.py
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.py
@@ -2,16 +2,16 @@ class Solution:
     def secondsBetweenTimes(self, startTime: str, endTime: str) -> int:
         st,et = startTime.split(':'), endTime.split(':')
         diff = [int(et[i])-int(st[i]) for i in range(3)]
-        print(diff)
-        if diff[-1] < 0:
-            diff[-1] = 60 + diff[-1]
-            diff[-2] -= 1
-            if diff[-2] < 0:
-                diff[-2] = 60 + diff[-2]
-                diff[-3] -= 1
-        else:
-            if diff[-2] < 0:
-                diff[-2] = 60 + diff[-2]
-                diff[-3] -= 1
+        # print(diff)
+        # if diff[-1] < 0:
+        #     diff[-1] = 60 + diff[-1]
+        #     diff[-2] -= 1
+        #     if diff[-2] < 0:
+        #         diff[-2] = 60 + diff[-2]
+        #         diff[-3] -= 1
+        # else:
+        #     if diff[-2] < 0:
+        #         diff[-2] = 60 + diff[-2]
+        #         diff[-3] -= 1
         return diff[-1] + 60*diff[-2] + 3600*diff[-3]
         
