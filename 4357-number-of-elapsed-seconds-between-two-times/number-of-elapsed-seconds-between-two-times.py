de30abb74a98d15d5118b638e77f3cb0137a9e35
Vikrant Singh <113819133+vikrxnt1@users.noreply.github.com>
2026-08-03 00:52:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.93%) - LeetSync

--- 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.py
+++ 4357-number-of-elapsed-seconds-between-two-times/number-of-elapsed-seconds-between-two-times.py
@@ -13,16 +13,5 @@ class Solution:
             if diff[-2] < 0:
                 diff[-2] = 60 + diff[-2]
                 diff[-3] -= 1
-            
-        # print(diff)
-        # return -1
-
-        # for i in range(len(diff)):
-        #     if diff[i] < 0:
-        #         diff[i] = 60 + diff[i]
-        #     if diff[i] > 0:
-        #         diff[i] -= 1
-        # diff[-1] += 1
-        # print (diff)
         return diff[-1] + 60*diff[-2] + 3600*diff[-3]
         
